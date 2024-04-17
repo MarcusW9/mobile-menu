b8d484fc928bca22afcfeef921848ffc59288777
Marcus W <marcuswsywong@gmail.com>
2024-04-17 10:26:58 +0100

    Add base dropdown menu
    dropdown menu with login form
    Javascript logic to trigger the dropdown

--- src/index.js
+++ src/index.js
@@ -1,25 +1,18 @@
-import _ from "lodash";
+// import _ from "lodash";
 import "./style.css";
 
-console.log("hello world");
+document.addEventListener('click', e => {
+  const isDropdownButton = e.target.matches('[data-dropdown-button]')
+  if (!isDropdownButton && e.target.closest('[data-dropdown]') != null) return 
+  
+  let currentDropdown
+  if (isDropdownButton) {
+    currentDropdown = e.target.closest('[data-dropdown]')
+    currentDropdown.classList.toggle('active')
+  }
 
-const name = "James";
-
-const person = { first: name };
-
-console.log(person);
-
-const sayHelloLinting = (fName) => {
-  console.log(`Hello linting, ${fName}`);
-};
-
-function component() {
-  const element = document.createElement("div");
-
-  // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = _.join(["Hello", "webpack"], " ");
-
-  return element;
-}
-
-document.body.appendChild(component());
+  document.querySelectorAll('[data-dropdown].active').forEach(dropdown => {
+    if (dropdown === currentDropdown) return
+    dropdown.classList.remove('active')
+  })
+})
